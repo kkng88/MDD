97498c211a60677ff77eb0b9b81a78a923c728af
kkng88 <kkng_882000@hotmail.com>
2013-09-08 10:36:32 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="utf-8">
-    <title>Language Learning</title>
-     <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta charset="utf-8"><title>Pictionary</title>
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-status-bar-style" content="black" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
